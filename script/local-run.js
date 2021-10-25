fd92480e818458293b6b857e4dc09f94d7d6a626
Danny Zhong <lesarde@gmail.com>
2021-10-25 19:33:06 +0800

    fixed local-run.js in windows platform.

--- script/local-run.js
+++ script/local-run.js
@@ -1,7 +1,7 @@
 const execSync = require('child_process').execSync;
 const { INIT_CWD, PWD } = process.env;
 
-if (!INIT_CWD || INIT_CWD === PWD || INIT_CWD.indexOf(PWD) === 0) {
+if (!INIT_CWD || INIT_CWD === PWD || INIT_CWD.toString().replace(/\\/g, () => '/').indexOf(PWD) === 0) {
   const command = ['npx', ...process.argv.slice(2, process.argv.length)].join(' ');
   execSync(command, { stdio: 'inherit' });
 }
